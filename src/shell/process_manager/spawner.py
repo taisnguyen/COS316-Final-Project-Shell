08a491cfd9cc03023db9cf9f2dc10deb80cc5b48
taisnguyen <tai.sanh.nguyen@fusionsystems.co.jp>
2023-12-12 23:06:49 -0500

    feat: allow multiple binary directories.

--- src/shell/process_manager/spawner.py
+++ src/shell/process_manager/spawner.py
@@ -1,38 +1,53 @@
 """Spawns and manages processes."""
 
 import os
+import asyncio
 import subprocess
 import psutil
-
-# Keep track of current subprocess.
-CURRENT_S_PROCESS = None
-
-# Keep track of all alive processes.
-# ALIVE_PROCESSES = []
+import shlex
 
 
 def spawn_process(args, exec_bin_path):
     """Spawns a process with the given arguments."""
 
+    # Keep track of current subprocess.
+    current_s_process = None
+    print("args", args)
+
     try:
         run_in_background = False
-        args = args.split(" ")
+        args = shlex.split(args)
 
-        # Run process in background.
+        # Run process in background
         if args[-1] == "&":
             run_in_background = True
             args = args[:-1]
 
-        CURRENT_S_PROCESS = subprocess.Popen(
-            [os.path.join(exec_bin_path, args[0]), *args[1:]])
+        # Try to start a process for each path to find the first valid one
+        bin_paths = exec_bin_path.split(";")
+        succeeded = False
+
+        for path in bin_paths:
+            try:
+                current_s_process = subprocess.Popen(
+                    [os.path.join(path, args[0]), *args[1:]])
+                succeeded = True
+                break
+            except Exception as err:
+                # Ignore the error since it is handled afterwards
+                pass
+
+        # Stop if no vaid paths were found
+        if not succeeded:
+            raise Exception()
 
         # Stall until process is finished if not running in background.
-        while not run_in_background and CURRENT_S_PROCESS.poll() is None:
+        while not run_in_background and current_s_process.poll() is None:
             pass
 
     except KeyboardInterrupt:
-        psutil.Process(CURRENT_S_PROCESS.pid).suspend()
+        psutil.Process(current_s_process.pid).suspend()
         print(
-            f'Process [{CURRENT_S_PROCESS.pid}] suspended. To resume, run "prs {CURRENT_S_PROCESS.pid}".')
-    except:
+            f'Process [{current_s_process.pid}] suspended. To resume, run "prs {current_s_process.pid}".')
+    except Exception:
         raise
